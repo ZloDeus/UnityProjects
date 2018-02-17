3a906106a9288913b28f03d30b481307d9eb37da
ZloDeus <34862040+ZloDeus@users.noreply.github.com>
2018-02-18 09:52:51 +1000

    Update 1.2
    Now this game have a little bit of gameplay

---
+++ SpaceShoter/Assets/Scripts/EnemyFireControll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireControll : MonoBehaviour
+{
+    public GameObject shot;
+    public Transform ShotSpawn;
+
+    private GameObject shotClone;
+    private float fireRate;
+    private float nextFire;
+
+    AudioSource fireSound;
+
+    private void Start()
+    {
+        fireRate = 0.25f;
+    }
+
+    void Update()
+    {
+        if ((Time.time > nextFire) && !GameController.GameIsPaused)
+        {
+            shot.transform.position = ShotSpawn.transform.position;
+            shotClone = Instantiate(shot) as GameObject;
+            nextFire = Time.time + fireRate;
+            fireSound = GetComponent<AudioSource>();
+            fireSound.Play();
+            
+        }
+    }
+
+}
---
+++ SpaceShoter/Assets/Scripts/ProjectileStartPositionRandomiser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileStartPositionRandomiser : MonoBehaviour
+{
+    public GameObject ProjectileClone;
+    Rigidbody rbClone;
+
+    private void Update()
+    {
+        rbClone = ProjectileClone.GetComponent<Rigidbody>();
+        float CloneShift = new float();
+        CloneShift = Random.Range(-5, 5);
+
+        rbClone.AddForce(CloneShift * 2, 0.0f, 0.0f);
+    }
+
+}
